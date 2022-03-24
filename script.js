fc77e6c15d69375c535df370d10cc55bd9eb762b
Amir <70907990+Amirka2@users.noreply.github.com>
2022-03-24 17:58:50 +0500

    появление менюшки по нажатию

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-let l = 0;
+document.getElementById("catalogue_button").addEventListener("click", () => {
+document.getElementById("main_left_menu").classList.toggle("visible")
+})
+document.getElementById("catalogue_title").addEventListener("click", () => {
+document.getElementById("main_left_menu").classList.toggle("visible")
+})
