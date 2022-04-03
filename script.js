76d744ba6037a72a2044e9049bd642d5d4c2deee
Amir <70907990+Amirka2@users.noreply.github.com>
2022-04-04 00:04:49 +0500

    меню готово / размеры страницы поменяны
    хедер не переделан

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-let l = 0;
+document.getElementById("catalogue_button").addEventListener("click", () => {
+document.getElementById("main_left_menu").classList.toggle("visible")
+})
+document.getElementById("catalogue_title").addEventListener("click", () => {
+document.getElementById("main_left_menu").classList.toggle("visible")
+})
+
+// document.getElementById("bath").addEventListener("click", () => {
+//   document.getElementById("bath_submenu").classList.toggle("visible")
+// })
+// document.getElementById("shower").addEventListener("click", () => {
+//   document.getElementById("shower_submenu").classList.toggle("visible")
+// })
+// document.getElementById("bathroom_furn").addEventListener("click", () => {
+//   document.getElementById("bathroom_furn_submenu").classList.toggle("visible")
+// })
+// document.getElementById("taps").addEventListener("click", () => {
+//   document.getElementById("taps_submenu").classList.toggle("visible")
+// })
+// document.getElementById("sanitary_ware").addEventListener("click", () => {
+//   document.getElementById("sanitary_ware_submenu").classList.toggle("visible")
+// })
+// document.getElementById("bathroom_accessories").addEventListener("click", () => {
+//   document.getElementById("bathroom_accessories_submenu").classList.toggle("visible")
+// })
+// document.getElementById("towel").addEventListener("click", () => {
+//   document.getElementById("towel_submenu").classList.toggle("visible")
+// })
+// document.getElementById("instal_system").addEventListener("click", () => {
+//   document.getElementById("instal_system_submenu").classList.toggle("visible")
+// })
+// document.getElementById("component").addEventListener("click", () => {
+//   document.getElementById("components_submenu").classList.toggle("visible")
+// })
+// document.getElementById("furniture_decor").addEventListener("click", () => {
+//   document.getElementById("furniture_decor_submenu").classList.toggle("visible")
+// })
+
+// попытка оптимизировать код
+// let liId;
+// window.onload = function() {
+//     document.body.onclick = function(event) {
+//          t=event.target;
+//          liId = t.id;
+//          alert(liId);
+//     }
+// }
